4cf8e48827c7e1fd53517a8680115866e1e1dca2
Arizal Assyafiq <arizalassyafiq@gmail.com>
2025-11-13 10:25:20 +0700

    first commit arizal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>KuloSehat - Informasi Kesehatan Terpercaya</title>
+    <!-- aruzal -->
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
