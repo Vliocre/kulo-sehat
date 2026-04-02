3e1b21273061f182de1f7e1476a3ae79ba78ddaf
Arizal Assyafiq <arizalassyafiq@gmail.com>
2026-04-02 09:11:50 +0700

    fix nested repo

--- app/Models/Illness.php
+++ app/Models/Illness.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Illness extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'age_category'
+    ];
 }
